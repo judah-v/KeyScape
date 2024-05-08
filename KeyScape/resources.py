1eea2b06b7ebb344ca3357aab89dfbbdafea589d
judah-v <90010677+judah-v@users.noreply.github.com>
2024-05-08 11:11:18 -0400

    Add files via upload

---
+++ KeyScape/resources.py
@@ -0,0 +1,31 @@
+import itertools
+data_folder = 'C:\\Users\\judah\\Desktop\\Python\\KeyScape\\'
+data_filename = 'user_data.txt'
+
+data = open(data_folder+data_filename).readlines()
+
+exec(f"filenames = {data[0]}")
+exec(f"save_data = {data[1]}")
+
+SOURCES = {}
+
+for filename, data in zip(filenames, save_data):
+    SOURCES[filename] = [data_folder + filename, data]
+
+NONE_TYPABLES = ['Shift_L', 'Shift_R', 'Caps_Lock', 'Control_L','Control_R', 'Win_L', 'Alt_L', 'Alt_R', 'Control_R', 'Scroll_Lock', 'Pause', 'Insert', 'Home', 'Prior', 'Delete', 'End', 'Next', 'Escape', 'F1', 'F2', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8', 'F9', 'F10', 'F11', 'F12', 'XF86Launch0', '??', 'XF86Mail', 'Num_Lock']
+
+SAMPLE_SIZE = data['segment_size']
+
+CURSOR = {'width': 8, 'height': 14}
+
+WIN = {
+    'x': 1000, 
+    'y': 50, 
+    'width': 500, 
+    'height': 500, 
+    'min_width': 300,
+    'min_height': (SAMPLE_SIZE+6)*CURSOR['height'],
+    'x_padding': 17, 
+    'y_padding': 5,
+    'b_padding': 35,
+   }
